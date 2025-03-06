634112a8f101d1ac4c8ab95d6dd9c413f7a9ced8
Juli1ou2 <julien.mrss@gmail.com>
2025-03-06 14:10:36 +0100

    fix material icons

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
+/**
+ * @title Home page component
+ */
 @Component({
   selector: 'app-home',
   imports: [MatSlideToggleModule, MatIconModule],
