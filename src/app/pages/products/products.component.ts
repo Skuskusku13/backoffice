efd8ef3ff3e5b0977e272e14e8c2424151a1f832
Juli1ou2 <julien.mrss@gmail.com>
2025-03-11 09:59:53 +0100

    feat add products tabs

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 /**
  * @title Products page component
  */
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  categories = [
+    { id: 0, name: 'Poissons' },
+    { id: 1, name: 'Crustacés' },
+    { id: 2, name: 'Fruits de mer' },
+  ];
+}
