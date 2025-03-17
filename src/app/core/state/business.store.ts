a7e318136a8cdb1fd6f85065dddfa01f3c201dca
Juli1ou2 <julien.mrss@gmail.com>
2025-03-17 16:01:05 +0100

    feat create transactions service, interface and store

---
+++ src/app/core/state/business.store.ts
@@ -0,0 +1,146 @@
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { Transaction } from '../models/transaction.interface';
+import { computed, inject } from '@angular/core';
+import { TransactionsService } from '../services/transactions.service';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { tapResponse } from '@ngrx/operators';
+import { pipe, debounceTime, tap, switchMap } from 'rxjs';
+
+type BusinessState = {
+  transactions: Transaction[];
+  isLoading: boolean;
+  revenueFilter: {
+    time: 'year' | 'quarter' | 'month' | 'week' | 'day';
+    category: number;
+    sale: 'all' | boolean;
+  };
+};
+
+const initialState: BusinessState = {
+  transactions: [],
+  isLoading: false,
+  revenueFilter: { time: 'month', category: -1, sale: 'all' },
+};
+
+export const BusinessStore = signalStore(
+  withState(initialState),
+  withComputed(({ transactions, revenueFilter }) => ({
+    revenue: computed(() => {
+      const today = new Date();
+      let amount = 0;
+      for (let transaction of transactions()) {
+        if (
+          transaction.type === 'retraitVente' &&
+          transaction.category === revenueFilter().category &&
+          (revenueFilter().sale === 'all' ||
+            transaction.onSale === revenueFilter().sale)
+        ) {
+          switch (revenueFilter().time) {
+            case 'year': {
+              if (transaction.date.getFullYear() === today.getFullYear()) {
+                amount += transaction.price;
+              }
+              break;
+            }
+            case 'quarter': {
+              const currentQuarter = Math.floor(today.getMonth() / 3);
+              const transactionQuarter = Math.floor(
+                transaction.date.getMonth() / 3
+              );
+              if (
+                transaction.date.getFullYear() === today.getFullYear() &&
+                transactionQuarter === currentQuarter
+              ) {
+                amount += transaction.price;
+              }
+              break;
+            }
+            case 'month': {
+              if (
+                transaction.date.getFullYear() === today.getFullYear() &&
+                transaction.date.getMonth() === today.getMonth()
+              ) {
+                amount += transaction.price;
+              }
+              break;
+            }
+            case 'week': {
+              const startOfWeek = new Date(today);
+              startOfWeek.setDate(today.getDate() - today.getDay());
+              const endOfWeek = new Date(startOfWeek);
+              endOfWeek.setDate(startOfWeek.getDate() + 6);
+              if (
+                transaction.date >= startOfWeek &&
+                transaction.date <= endOfWeek
+              ) {
+                amount += transaction.price;
+              }
+              break;
+            }
+            case 'day': {
+              if (
+                transaction.date.getFullYear() === today.getFullYear() &&
+                transaction.date.getMonth() === today.getMonth() &&
+                transaction.date.getDate() === today.getDate()
+              ) {
+                amount += transaction.price;
+              }
+              break;
+            }
+          }
+        }
+      }
+      return amount;
+    }),
+    // revenueBis: computed(() => revenue()+3),
+  })),
+  withMethods((store) => ({
+    updateTimeFilter(
+      time: 'year' | 'quarter' | 'month' | 'week' | 'day'
+    ): void {
+      patchState(store, (state) => ({
+        revenueFilter: { ...state.revenueFilter, time },
+      }));
+    },
+    updateCategoryFilter(category: number): void {
+      patchState(store, (state) => ({
+        revenueFilter: { ...state.revenueFilter, category },
+      }));
+    },
+    updateSaleFilter(sale: 'all' | boolean): void {
+      patchState(store, (state) => ({
+        revenueFilter: { ...state.revenueFilter, sale },
+      }));
+    },
+  })),
+  withMethods((store, booksService = inject(TransactionsService)) => ({
+    load: rxMethod<void>(
+      pipe(
+        debounceTime(300),
+        // distinctUntilChanged(),
+        tap(() => patchState(store, { isLoading: true })),
+        switchMap(() => {
+          return booksService.getTransactions().pipe(
+            tapResponse({
+              next: (transactions) => {
+                patchState(store, { transactions, isLoading: false });
+                console.log(store.transactions());
+                console.log(store.revenue());
+              },
+              error: (err) => {
+                patchState(store, { isLoading: false });
+                console.error(err);
+              },
+            })
+          );
+        })
+      )
+    ),
+  }))
+);
--- src/app/pages/business/business.component.ts
+++ src/app/pages/business/business.component.ts
@@ -1,7 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {GraphCardComponent} from '../../shared/graph-card/graph-card.component';
-import {MatGridList, MatGridListModule, MatGridTile} from '@angular/material/grid-list';
-import {FilterComponent} from '../../shared/filter/filter.component';
+import { GraphCardComponent } from '../../shared/graph-card/graph-card.component';
+import {
+  MatGridList,
+  MatGridListModule,
+  MatGridTile,
+} from '@angular/material/grid-list';
+import { FilterComponent } from '../../shared/filter/filter.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { BusinessStore } from '../../core/state/business.store';
 
 /**
  * @title Business page component
@@ -13,52 +18,55 @@ import {FilterComponent} from '../../shared/filter/filter.component';
     MatGridList,
     MatGridTile,
     MatGridListModule,
-    FilterComponent
+    FilterComponent,
   ],
+  providers: [BusinessStore],
   templateUrl: './business.component.html',
   styleUrl: './business.component.scss',
 })
 export class BusinessComponent implements OnInit {
-
+  readonly store = inject(BusinessStore);
   data: any = [
     {
       title: "Chiffre d'affaire",
       sales: 20,
-      valuePercent: 0
-    }, {
-      title: "Résultat comptable",
-      sales: 14,
-      valuePercent: 0
-    }, {
-      title: "Impôt",
-      sales: 6,
-      valuePercent: 0
+      valuePercent: 0,
     },
-  ]
+    {
+      title: 'Résultat comptable',
+      sales: 14,
+      valuePercent: 0,
+    },
+    {
+      title: 'Impôt',
+      sales: 6,
+      valuePercent: 0,
+    },
+  ];
 
   filters = [
-    {value: 'year', viewValue: 'Annuel', titleForm: 'Période'},
-    {value: 'quarter', viewValue: 'Trimestriel'},
-    {value: 'month', viewValue: 'Mensuel'},
-    {value: 'week', viewValue: 'Hebdomadaire'},
-    {value: 'day', viewValue: 'Journalier'},
+    { value: 'year', viewValue: 'Annuel', titleForm: 'Période' },
+    { value: 'quarter', viewValue: 'Trimestriel' },
+    { value: 'month', viewValue: 'Mensuel' },
+    { value: 'week', viewValue: 'Hebdomadaire' },
+    { value: 'day', viewValue: 'Journalier' },
   ];
 
   categories = [
-    {value: 'all', viewValue: 'Tous', titleForm: 'Catégories'},
-    {value: '0', viewValue: 'Poissons'},
-    {value: '1', viewValue: 'Fruits De Mer'},
-    {value: '2', viewValue: 'Crustacés'},
+    { value: 'all', viewValue: 'Tous', titleForm: 'Catégories' },
+    { value: '0', viewValue: 'Poissons' },
+    { value: '1', viewValue: 'Fruits De Mer' },
+    { value: '2', viewValue: 'Crustacés' },
   ];
 
   typeVentes = [
-    {value: 'all', viewValue: 'Tous', titleForm: 'Types de ventes'},
-    {value: 'false', viewValue: 'Achats'},
-    {value: 'true', viewValue: 'Promotions'},
+    { value: 'all', viewValue: 'Tous', titleForm: 'Types de ventes' },
+    { value: 'false', viewValue: 'Achats' },
+    { value: 'true', viewValue: 'Promotions' },
   ];
 
   ngOnInit(): void {
-    console.log(this.data)
+    this.store.load();
+    console.log(this.data);
   }
-
 }
