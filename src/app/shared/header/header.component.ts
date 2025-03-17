31eb8d7e571cbae0a59ff71bd7fb07ee3e3bae3b
Juli1ou2 <julien.mrss@gmail.com>
2025-03-17 16:12:11 +0100

    feat add logo SVG icon to header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,17 @@
-import {Component, inject, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {Router, RouterLinkActive, RouterModule} from '@angular/router';
-import {LoginService} from '../../core/services/login.service';
+import {
+  Component,
+  inject,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router, RouterLinkActive, RouterModule } from '@angular/router';
+import { LoginService } from '../../core/services/login.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 /**
  * @title Footer component
@@ -21,9 +28,8 @@ import {LoginService} from '../../core/services/login.service';
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
- })
+})
 export class HeaderComponent implements OnInit {
-
   loginService: LoginService = inject(LoginService);
   router: Router = inject(Router);
   loggedIn!: boolean;
@@ -31,19 +37,24 @@ export class HeaderComponent implements OnInit {
   navBarItems = [
     { name: 'BackOffice TIG', segment: 'home' },
     { name: 'Products', segment: 'products' },
-    { name: 'Business', segment: 'business' }
+    { name: 'Business', segment: 'business' },
   ];
+
+  constructor() {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
+    iconRegistry.addSvgIcon('logo', sanitizer.bypassSecurityTrustResourceUrl('dessin.svg'));
+  }
 
   ngOnInit(): void {
     this.loginService.isLoggedIn.subscribe((value: boolean) => {
-      console.log(value)
-      this.loggedIn = value
-    })
+      console.log(value);
+      this.loggedIn = value;
+    });
   }
 
   logout() {
     this.loginService.logout();
     this.router.navigate(['/login']);
   }
-
 }
