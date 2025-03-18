fb9437af5c638e5caea4550626870a3367b62add
Juli1ou2 <julien.mrss@gmail.com>
2025-03-18 13:58:54 +0100

    fix: rename SpinnerComponent selector

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
-  selector: 'app-spinner',
+  selector: 'hand-spinner',
   imports: [MatProgressSpinnerModule],
   templateUrl: './spinner.component.html',
   styleUrl: './spinner.component.scss',
