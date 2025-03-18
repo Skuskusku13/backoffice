12917b10437c5ee42adb9bd12bf7f5d718835863
Juli1ou2 <julien.mrss@gmail.com>
2025-03-18 11:40:39 +0100

    fix: update snackbar

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -5,13 +5,19 @@ import { ProductsStore } from '../../core/state/products.store';
 import { ProductUpdateData } from '../../core/models/product-update-dto.interface';
 import { ProductsService } from '../../core/services/products.service';
 import { SpinnerComponent } from '../../shared/spinner/spinner.component';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 /**
  * @title Products page component
  */
 @Component({
   selector: 'app-products',
-  imports: [MatTabsModule, ProductsTableComponent, SpinnerComponent],
+  imports: [
+    MatTabsModule,
+    ProductsTableComponent,
+    SpinnerComponent,
+    MatSnackBarModule,
+  ],
   providers: [ProductsStore],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
@@ -25,6 +31,8 @@ export class ProductsComponent implements OnInit {
   readonly store = inject(ProductsStore);
   private productsService: ProductsService = inject(ProductsService);
 
+  constructor(private snackBar: MatSnackBar) {}
+
   ngOnInit(): void {
     this.store.load();
   }
@@ -33,11 +41,13 @@ export class ProductsComponent implements OnInit {
     this.productsService.updateProduct(productUpdateData).subscribe({
       next: () => {
         // alert('Produit modifié avec succès !');
+        this.openSnackBar('info');
         this.store.load();
       },
       error: (error) => {
-        console.log(error);
         // alert('Erreur lors de la modification du produit !' + error);
+        console.log(error);
+        this.openSnackBar('error');
       },
     });
   }
@@ -46,12 +56,28 @@ export class ProductsComponent implements OnInit {
     this.productsService.updateProducts(productsUpdateData).subscribe({
       next: () => {
         // alert('Produits modifiés avec succès !');
+        this.openSnackBar('info');
         this.store.load();
       },
       error: (error) => {
-        console.log(error);
         // alert('Erreur lors de la modification des produits !' + error);
+        console.log(error);
+        this.openSnackBar('error');
       },
+    });
+  }
+
+  openSnackBar(type: 'info' | 'error'): void {
+    const text =
+      type === 'info'
+        ? 'Produit modifié avec succès !'
+        : 'Erreur lors de la modification du produit !';
+    const panelClass = type === 'info' ? 'info-snackbar' : 'error-snackbar';
+    this.snackBar.open(text, 'Fermer', {
+      duration: 4000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+      panelClass: [panelClass],
     });
   }
 }
